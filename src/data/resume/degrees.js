71827523c262697f4be1fa41b28dcd1201a5153c
ArunaJewelson <arunadevasikannan25@gmail.com>
2025-02-28 01:24:54 +0000

    Initia cmt

---
+++ src/data/resume/degrees.js
@@ -0,0 +1,16 @@
+const degrees = [
+  {
+    school: 'Manchester Metropolitan University',
+    degree: 'M.S. Data Science',
+    link: 'https://www.mmu.ac.uk/',
+    year: 2021,
+  },
+  {
+    school: 'Anna University',
+    degree: 'B.E in Computer Science',
+    link: 'https://www.annauniv.edu/',
+    year: 2013,
+  },
+];
+
+export default degrees;
